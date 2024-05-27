6b693db960ecdf7a5144ada662e03355be7cc224
Shafiq Raees <malikshafiq7088@gmail.com>
2024-05-27 19:11:52 +0500

    corsUpdate

--- config/cors.php
+++ config/cors.php
@@ -15,16 +15,15 @@ return [
     |
     */
 
-    //'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'paths' => ['api/*', 'oauth/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    'allowed_methods' => ['*'],
 
     'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'X-CSRF-TOKEN'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
